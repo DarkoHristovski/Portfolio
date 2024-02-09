073353216a11fa774e5edd9a0fa15066b4e48b49
Darko Hristovski <hristovski15@gmail.com>
2024-02-09 19:16:01 +0100

    creating hero-section

---
+++ src/userProfil.js
@@ -0,0 +1,21 @@
+const developer = {
+    name: "Darko",
+    lastName:"Hristovski",
+    about: "A passionate web developer with expertise in front-end and back-end technologies.",
+    email: "hristovski15@gmail.com",
+    projects: [
+      {
+        title: "Project 1",
+        description: "A responsive website built using HTML, CSS, and JavaScript. Implemented modern design principles and animations for a sleek user experience.",
+        technologies: ["HTML", "CSS", "JavaScript"]
+      },
+      {
+        title: "Project 2",
+        description: "An e-commerce platform developed with React.js and Node.js. Integrated with Stripe API for secure payment processing.",
+        technologies: ["React.js", "Node.js", "Stripe API"]
+      },
+      // Add more projects as needed
+    ],
+    // You can add more properties such as education, experience, etc.
+  };
+  export default developer;
