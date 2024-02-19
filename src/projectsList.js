9e45c0a97dbe11a4de91aabe8f4aa52ca7ba5e1e
Darko Hristovski <hristovski15@gmail.com>
2024-02-19 13:59:28 +0100

    new changes

--- src/projectsList.js
+++ src/projectsList.js
@@ -1,23 +1,27 @@
+import fitlife from '../src/assets/fitlife.png'
+import weather from '../src/assets/todo-app.png'
+import todo from '../src/assets/todo-app.png'
+
 const projectsList=[
 
     {
       id:1,
         name:"FitLife",
-        img:'/assets/fitlife.png',
+        img:`${fitlife}`,
         description: "A fully functional e-commerce website built with React.js and Node.js. Integrated with a payment gateway for secure transactions.",
         technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Stripe API"]
       },
       {
         id:2,
         name:"WeatherApp",
-        img:'/assets/todo-app.png',
+        img:`${weather}`,
         description: "A fully functional e-commerce website built with React.js and Node.js. Integrated with a payment gateway for secure transactions.",
         technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Stripe API"]
       },
       {
         id:3,
         name:"ToDoApp",
-        img:'/assets/fitlife.png',
+        img:`${todo}`,
         description: "A fully functional e-commerce website built with React.js and Node.js. Integrated with a payment gateway for secure transactions.",
         technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Stripe API"]
       }
