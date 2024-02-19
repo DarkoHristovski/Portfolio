a97fc68e7b6ba6ddff1c96c2a3b2e38ec1ca3c4e
Darko Hristovski <hristovski15@gmail.com>
2024-02-19 13:26:56 +0100

    changing img roots

--- src/projectsList.js
+++ src/projectsList.js
@@ -3,21 +3,21 @@ const projectsList=[
     {
       id:1,
         name:"FitLife",
-        img:'../src/assets/fitlife.png',
+        img:'/assets/fitlife.png',
         description: "A fully functional e-commerce website built with React.js and Node.js. Integrated with a payment gateway for secure transactions.",
         technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Stripe API"]
       },
       {
         id:2,
         name:"WeatherApp",
-        img:'../src/assets/todo-app.png',
+        img:'/assets/todo-app.png',
         description: "A fully functional e-commerce website built with React.js and Node.js. Integrated with a payment gateway for secure transactions.",
         technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Stripe API"]
       },
       {
         id:3,
         name:"ToDoApp",
-        img:'../src/assets/fitlife.png',
+        img:'/assets/fitlife.png',
         description: "A fully functional e-commerce website built with React.js and Node.js. Integrated with a payment gateway for secure transactions.",
         technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Stripe API"]
       }
