df906ae9871ab09f19987b6ef1d8785dec9eaa93
Darko Hristovski <hristovski15@gmail.com>
2024-05-24 02:53:27 +0200

    upoload new cv

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 
-import cv from "../../../src/Darko_Hristovski.pdf";
+import cv from "../../../src/assets/Darko Hristovski Lebenslauf.pdf";
 import "./header.css";
 
 const Header = () => {
